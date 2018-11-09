607d6ea076fe0c79bff29edc93708888b3f81a1a
Tim ONeil <interval1066@gmail.com>
2018-11-08 18:27:19 -0600

    Get main ready for command line stuff

--- pubsub/main.cc
+++ pubsub/main.cc
@@ -6,6 +6,8 @@
 int
 main(int argc, char** argv)
 {
+
+	// todo: grab command line params and open input, crearte output, pick an algorthim
     // Create 4 threads:
     ModuleA* thread1 = new ModuleA();
     ModuleA* thread2 = new ModuleA();
@@ -27,9 +29,6 @@ main(int argc, char** argv)
     thread3->setServerHandle(msg_server);
     thread4->setServerHandle(msg_server);
 
-    // Test: unsubscribe thread1 from all topics. The method of
-    // unsubscribeOneThreadFromAllTopics() can be invoked before destroying a
-    // thread, to clean up subscriber lists without pointing to dead threads
     msg_server->unsubscribeOneThreadFromAllTopics(thread1);
 
     // Waiting for 2 seconds
@@ -48,12 +47,6 @@ main(int argc, char** argv)
     usleep(10000000);
     // Unsubscribe thread4 from topic "Topic moduleB"
     msg_server->unsubscribe(thread4, "Topic moduleB");
-    std::cout << "-------------------------------------------------------------"
-    << std::endl;
-    std::cout << "-----------unsubscribe(thread4, Topic_for_moduleB)-----------"
-    << std::endl;
-    std::cout << "-------------------------------------------------------------"
-    << std::endl;
 
     // Waitting for 1 seconds
     usleep(1000000);
